63df19accac6457e6bb0689dd91f43542545faa0
ahmedakta <ahmdekta@gmail.com>
2025-03-02 22:55:08 +0300

    route changes

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,53 @@
 const express = require("express");
 const router = express.Router();
+// middleware in all routes
+router.use(logger);
 
-// router
-// routes
-router.get("/", (req, res) => {
+router.get("/", logger, (req, res) => {
   res.send("Users List");
 });
-router.get("/new", (req, res) => {
-  res.send("User Form");
+
+router.post("/", (req, res) => {
+  const isValid = false;
+  if (isValid) {
+    users.push({ firstName: req.body.firstname });
+    res.redirect(`/users/${users.length - 1}`);
+  } else {
+    console.log("error");
+    res.render("users/new", { firstname: req.body.firstname });
+  }
 });
 
+router.get("/new", (req, res) => {
+  res.render("users/new");
+});
+
+router.post("/create", (req, res) => {
+  res.send("Create User");
+});
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    console.log(req.user);
+    res.send(`Get user with ID : ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`User Edit ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`User Edit ${req.params.id}`);
+  });
+
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
+
+//  middleware
+const users = [{ name: "Ahmed" }, { name: "saly" }];
+router.param("id", (req, res, next, id) => {
+  req.user = users[id];
+  next();
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,22 @@
 const express = require("express");
 // setup actual server
 const app = express();
+
+// render static files renderer middleware
+app.use(express.static("public"));
+// tu access body of requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // for json request
+
 app.set("view engine", "ejs");
 
 const userRoutes = require("./routes/users");
 const postRouter = require("./routes/posts");
 
 app.get("/", (req, res) => {
-  res.send("Home Page");
+  res.render("index");
 });
 
 app.use("/users", userRoutes);
-app.use("/posts", userRoutes);
+app.use("/posts", postRouter);
 app.listen(3000);
